627f4dacc8b01672bbf9aa446d0daa21abb381cb
janbegjan <janbeg2014@gmail.com>
2018-10-18 11:36:50 +0600

    dynamic index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <?php wp_head(); ?>
 <body <?php body_class(); ?>>
 <div class="header">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-              <h3 class="tagline">No act of kindness, no matter how small, is ever wasted</h3>
-              <h1 class="align-self-center display-1 text-center heading">Hello Beautiful</h1>
+              <h3 class="tagline"><?php bloginfo('description'); ?></h3>
+              <h1 class="align-self-center display-1 text-center heading"><?php bloginfo('title'); ?></h1>
             </div>
         </div>
     </div>
