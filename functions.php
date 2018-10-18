20118fbcb7e3c575158b441fd2d472a042a80bf1
janbegjan <janbeg2014@gmail.com>
2018-10-18 11:28:05 +0600

    theme assets

--- functions.php
+++ functions.php
@@ -11,3 +11,14 @@ function wpAlpha_bootstrapping(){
   register_nav_menu('Footer_menu', __('Footer menu', 'wpAlpha'));
 }
 add_action('after_setup_theme', 'wpAlpha_bootstrapping');
+/**
+* wpAlpha theme assets
+*/
+function wpAlpha_assets(){
+  //stylesheets
+  wp_enqueue_style( 'bootstrap-css', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' );
+  wp_enqueue_style('wpAlpha_stylesheet-css', get_stylesheet_uri(), null, time());
+  //scripts
+  wp_enqueue_script('wpAlpha-main-js', get_theme_file_uri('/assets/js/main.js' ), array('jquery'), time(), true);
+}
+add_action('wp_enqueue_scripts', 'wpAlpha_assets');
