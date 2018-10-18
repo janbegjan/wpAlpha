b07c716766fa718b40a7e0ed0d58dae9678a150b
janbegjan <janbeg2014@gmail.com>
2018-10-18 11:23:14 +0600

    neccessary bootstrapping

--- functions.php
+++ functions.php
@@ -1,3 +1,13 @@
 <?php
 
-?>
+/**
+* wpAlpha theme bootstrap
+*/
+function wpAlpha_bootstrapping(){
+  load_theme_textdomain( 'wpAlpha' );
+  add_theme_support( 'title-tag' );
+  add_theme_support( 'post-thumbnails' );
+  register_nav_menu('Top_menu', __('Top menu', 'wpAlpha'));
+  register_nav_menu('Footer_menu', __('Footer menu', 'wpAlpha'));
+}
+add_action('after_setup_theme', 'wpAlpha_bootstrapping');
