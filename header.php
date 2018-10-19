f2b313917d3aa827fd8954ef5e27a8f0c4682fa2
janbegjan <janbeg2014@gmail.com>
2018-10-19 10:58:54 +0600

    adding hero.php header

--- header.php
+++ header.php
@@ -4,13 +4,4 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <?php wp_head(); ?>
 <body <?php body_class(); ?>>
-<div class="header">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-              <h3 class="tagline"><?php bloginfo('description'); ?></h3>
-              <h1 class="align-self-center display-1 text-center heading"><a href="<?php echo get_home_url(); ?>"><?php bloginfo('title'); ?></a></h1>
-            </div>
-        </div>
-    </div>
-</div>
+<?php get_template_part('hero'); ?>
