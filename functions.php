782f94b5410c38836d4483cb3f2f5b64321b5ccd
janbegjan <janbeg2014@gmail.com>
2018-10-21 19:08:16 +0600

    right sidebar added

--- functions.php
+++ functions.php
@@ -22,3 +22,18 @@ function wpAlpha_assets(){
   wp_enqueue_script('wpAlpha-main-js', get_theme_file_uri('/assets/js/main.js' ), array('jquery'), time(), true);
 }
 add_action('wp_enqueue_scripts', 'wpAlpha_assets');
+/**
+* Add Right sidebar register.
+*/
+function wpAlpha_register_sidebar() {
+  register_sidebar( array(
+  'name'          => __( 'Right Sidebar', 'wpAlpha' ),
+  'id'            => 'sidebar_right',
+  'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'wpAlpha' ),
+  'before_widget' => '<section id="%1s" class="widget %2s">',
+  'after_widget'  => '</section>',
+  'before_title'  => '<h2 class="widget-title">',
+  'after_title'   => '</h2>',
+  ));
+}
+add_action( 'widgets_init', 'wpAlpha_register_sidebar' );
--- single.php
+++ single.php
@@ -1,61 +1,66 @@
 <?php get_header(); ?>
-<div class="posts">
-  <?php
-    while(have_posts()):
-      the_post();
-  ?>
-
-    <div class="post">
+<div class="single-post row">
+  <div class="col-md-8">
+    <div class="posts">
+      <?php
+        while(have_posts()):
+          the_post();
+      ?>
+      <div class="post">
         <div class="container">
-            <div class="row">
-                <div class="col-md-10 offset-md-1">
-                    <h2 class="post-title text-center"><?php the_title(); ?></h2>
-                    <p class="text-center">
-                        <strong><?php the_author(); ?></strong><br/>
-                       <?php echo get_the_date(); ?>
-                    </p>
-                    <div class="text-center">
-                      
-                      <?php  
-                        if(get_the_tag_list()) {
-                            echo get_the_tag_list('<ul class="list-unstyled"><li>','</li><li>','</li></ul>');
-                        }
-                        ?>
-                  </div>
-                </div>
-            </div>
-            <div class="row">
-                
-                <div class="col-md-10 offset-md-1">
-                  
-                    <p>
-                      <?php 
-                        if(has_post_thumbnail() ){
-                          the_post_thumbnail( 'large', 'class=img-fluid' );
-                        }
-                      ?>
-                    </p>
-                    <?php the_content(); ?>
-                  
-                </div>
-                <div class="col-md-10 offset-md-1">
-                  <span class="text-left"><?php next_post_link(); ?>
-                  <span class="text-right"><?php previous_post_link(); ?></span> 
-                </div>
-                   
-                <?php 
-                  if(comments_open()):
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="post-title"><?php the_title(); ?></h2>
+              <p>
+                <strong><?php the_author(); ?></strong><br/>
+                <?php echo get_the_date(); ?>
+              </p>
+              <p>
+                <?php  
+                  if(get_the_tag_list()) {
+                      echo get_the_tag_list('<ul class="list-unstyled"><li>','</li><li>','</li></ul>');
+                  }
                 ?>
-                <div class="comments_form">
-                  <?php comments_template(); ?>
-                </div>
-                <?php endif; ?>
-
+              </p>
             </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <p>
+                <?php 
+                  if(has_post_thumbnail() ){
+                    the_post_thumbnail( 'large', 'class=img-fluid' );
+                  }
+                ?>
+              </p>
+              <?php the_content(); ?>
+                    
+            </div>
+            <div class="col-md-12">
+              <span class="text-left"><?php next_post_link(); ?>
+              <span class="text-right"><?php previous_post_link(); ?></span> 
+            </div>        
+              <?php 
+                if(comments_open()):
+              ?>
+              <div class="comments_form">
+                <?php comments_template(); ?>
+              </div>
+              <?php endif; ?>
+
+          </div>
 
         </div>
+      </div>
+      <?php endwhile; ?>
     </div>
-  <?php endwhile; ?>
-  
+  </div>
+  <div class="col-md-4">
+    <?php 
+      if(is_active_sidebar( 'sidebar_right' )){
+        dynamic_sidebar( 'sidebar-right' );
+      }
+    ?>
+  </div>
 </div>
 <?php get_footer(); ?>
