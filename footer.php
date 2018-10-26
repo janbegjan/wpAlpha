0429da162926ad97b52cb346945da63aca27e870
janbegjan <janbeg2014@gmail.com>
2018-10-26 14:04:15 +0600

    phpcs

--- footer.php
+++ footer.php
@@ -1,28 +1,29 @@
 <div class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-            <?php 
-              if(is_active_sidebar( 'sidebar_footer' )){
-                dynamic_sidebar( 'sidebar_footer' );
-              }
-            ?>
-            </div>
-            <div class="col-md-6">
-            <?php 
-              /**
-              *Adding theme footermenu
-              */
-              wp_nav_menu(array(
-                'theme_location'=>'footermenu',
-                'menu_id'=>'footermenu_container',
-                'menu_class'=>'list-inline text-center',
-              ));
-            ?>
-            </div>
-        </div>
-    </div>
-</div>
-<?php wp_footer(); ?>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<?php
+						if ( is_active_sidebar( 'sidebar_footer' ) ) {
+							dynamic_sidebar( 'sidebar_footer' );
+						}
+					?>
+				</div>
+				<div class="col-md-6">
+					<?php
+						/**
+						*Adding theme footermenu
+						*/
+						wp_nav_menu( array(
+							'theme_location' => 'footermenu',
+							'menu_id' => 'footermenu_container',
+							'menu_class' => 'list-inline text-center',
+						)
+						);
+					?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php wp_footer(); ?>
 </body>
 </html>
