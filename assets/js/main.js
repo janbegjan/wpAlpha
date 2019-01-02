b46a2bf442258ac910a20db477832ce4ee10966d
janbegjan <janbeg2014@gmail.com>
2019-01-02 19:32:39 +0600

    lightbox showing with jQuery 3.22

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
 // @ts-nocheck
-;(function($){
-
+;(function ($) {
+	$(document).ready(function(){
+		$(".popup").each(function(){
+			var image = $(this).find("img").attr("src");
+			$(this).attr("href", image);
+		})
+	});
 }(jQuery));
--- single.php
+++ single.php
@@ -1,69 +1,73 @@
 <?php get_header(); ?>
 <div class="single-post row">
-  <div class="col-md-8">
-    <div class="posts">
-      <?php
-        while(have_posts()):
-          the_post();
-      ?>
-      <div class="post">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12">
-              <h2 class="post-title"><?php the_title(); ?></h2>
-              <p>
-                <strong><?php the_author(); ?></strong><br/>
-                <?php echo get_the_date(); ?>
-              </p>
-              <p>
-                <?php  
-                  if(get_the_tag_list()) {
-                      echo get_the_tag_list('<ul class="list-unstyled"><li>','</li><li>','</li></ul>');
-                  }
-                ?>
-              </p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12">
-              <p>
-                <?php 
-                  $thumbnail_url = get_the_post_thumbnail_url(null, 'large');
-                  if(has_post_thumbnail() ){
-                    // echo '<a href="'.$thumbnail_url.'" data-featherlight="image">';
-                    printf('<a href="%s" data-featherlight="image">', $thumbnail_url);
-                    the_post_thumbnail( 'large', 'class=img-fluid' );
-                    echo '</a>';
-                  }
-                ?>
-              </p>
-              <?php the_content(); ?>
-                    
-            </div>
-            <div class="col-md-12">
-              <span class="text-left"><?php next_post_link(); ?>
-              <span class="text-right"><?php previous_post_link(); ?></span> 
-            </div>        
-              <?php 
-                if(comments_open()):
-              ?>
-              <div class="comments_form">
-                <?php comments_template(); ?>
-              </div>
-              <?php endif; ?>
-
-          </div>
-
-        </div>
-      </div>
-      <?php endwhile; ?>
-    </div>
-  </div>
+	<div class="col-md-8">
+		<div class="posts">
+			<?php
+				while ( have_posts()) :
+					the_post();
+			?>
+			<div class="post">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h2 class="post-title"><?php the_title(); ?></h2>
+							<p>
+								<strong><?php the_author(); ?></strong><br/>
+								<?php echo get_the_date(); ?>
+							</p>
+							<p>
+								<?php
+								if (get_the_tag_list()) {
+										echo get_the_tag_list('<ul class="list-unstyled"><li>', '</li><li>', '</li></ul>');
+								}
+								?>
+							</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<p>
+								<?php 
+									//method 1
+									// $thumbnail_url = get_the_post_thumbnail_url(null, 'large');
+									// if(has_post_thumbnail() ) {
+									// 	// echo '<a href="'.$thumbnail_url.'" data-featherlight="image">';
+									// 	printf('<a href="%s" data-featherlight="image">', $thumbnail_url);
+									// 	the_post_thumbnail('large', 'class=img-fluid');
+									// 	echo '</a>';
+									// }
+									//method 2 (jQuery 3.22)
+										if(has_post_thumbnail()){
+											echo '<a href ="#" data-featherlight="image" class="popup">';
+											the_post_thumbnail('large', 'class=img-fluid');
+											echo '</a>';
+										}
+								?>
+							</p>
+							<?php the_content(); ?>      
+						</div>
+						<div class="col-md-12">
+							<span class="text-left"><?php next_post_link(); ?>
+							<span class="text-right"><?php previous_post_link(); ?></span> 
+						</div>        
+						<?php 
+							if(comments_open()) :
+						?>
+						<div class="comments_form">
+							<?php comments_template(); ?>
+						</div>
+						<?php endif; ?>
+					</div>
+				</div>
+			</div>
+			<?php endwhile; ?>
+		</div>
+	</div>
   <div class="col-md-4">
     <?php 
-      if(is_active_sidebar( 'sidebar_right' )){
-        dynamic_sidebar( 'sidebar_right' );
-      }
+			if (is_active_sidebar('sidebar_right')) {
+				dynamic_sidebar('sidebar_right');
+			}
     ?>
   </div>
 </div>
