ad126451248dcb754ae10b1da628800533dd974b
janbegjan <janbeg2014@gmail.com>
2019-01-03 01:54:43 +0600

    fixed comments template

--- comments.php
+++ comments.php
@@ -1,88 +1,59 @@
 <?php
 /**
- * The template for displaying comments
+ * The template for displaying Comments.
  *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
+ * The area of the page that contains comments and the comment form.
  *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package    WordPress
- * @subpackage wpAlpha
- * @since      1.0
- * @version    1.0
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
  */
-
+ 
 /*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
+ * If the current post is protected by a password and the visitor has not yet
+ * entered the password we will return early without loading the comments.
  */
-if (post_password_required() ) {
+if ( post_password_required() )
     return;
-}
 ?>
-
+ 
 <div id="comments" class="comments-area">
-
-    <?php
-    // You can start editing here -- including this comment!
-    if (have_comments() ) :
-    ?>
-     <h2 class="comments-title">
+ 
+    <?php if ( have_comments() ) : ?>
+        <h2 class="comments-title">
+            <?php
+                printf( _nx( 'One thought on "%2$s"', '%1$s thoughts on "%2$s"', get_comments_number(), 'comments title', 'wpAlpha' ),
+                    number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+            ?>
+        </h2>
+ 
+        <ol class="comment-list">
+            <?php
+                wp_list_comments( array(
+                    'style'       => 'ol',
+                    'short_ping'  => true,
+                    'avatar_size' => 74,
+                ) );
+            ?>
+        </ol><!-- .comment-list -->
+ 
         <?php
-        $comments_number = get_comments_number();
-        if ('1' === $comments_number ) {
-            /* translators: %s: post title */
-            printf(_x('One Reply to &ldquo;%s&rdquo;', 'comments title', 'wpAlpha'), get_the_title());
-        } else {
-            printf(
-                /* translators: 1: number of comments, 2: post title */
-                _nx(
-                    '%1$s Reply to &ldquo;%2$s&rdquo;',
-                    '%1$s Replies to &ldquo;%2$s&rdquo;',
-                    $comments_number,
-                    'comments title',
-                    'wpAlpha'
-                ),
-                number_format_i18n($comments_number),
-                get_the_title()
-            );
-        }
+            // Are there comments to navigate through?
+            if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
         ?>
-     </h2>
-
-     <ol class="comment-list">
-        <?php
-        wp_list_comments(
-            array(
-            'avatar_size' => 100,
-            'style'       => 'ol',
-            'short_ping'  => true,
-            )
-        );
-        ?>
-     </ol>
-
-        <?php
-        the_comments_pagination(
-            array(
-            'prev_text' => wpAlpha_get_svg(array( 'icon' => 'arrow-left' )) . '<span class="screen-reader-text">' . __('Previous', 'wpAlpha') . '</span>',
-            'next_text' => '<span class="screen-reader-text">' . __('Next', 'wpAlpha') . '</span>' . wpAlpha_get_svg(array( 'icon' => 'arrow-right' )),
-            )
-        );
-
-    endif; // Check for have_comments().
-
-    // If comments are closed and there are comments, let's leave a little note, shall we?
-    if (! comments_open() && get_comments_number() && post_type_supports(get_post_type(), 'comments') ) :
-    ?>
-
-     <p class="no-comments"><?php _e('Comments are closed.', 'wpAlpha'); ?></p>
-    <?php
-    endif;
-
-    comment_form();
-    ?>
-
+        <nav class="navigation comment-navigation" role="navigation">
+            <h1 class="screen-reader-text section-heading"><?php _e( 'Comment navigation', 'wpAlpha' ); ?></h1>
+            <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'wpAlpha' ) ); ?></div>
+            <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'wpAlpha' ) ); ?></div>
+        </nav><!-- .comment-navigation -->
+        <?php endif; // Check for comment navigation ?>
+ 
+        <?php if ( ! comments_open() && get_comments_number() ) : ?>
+        <p class="no-comments"><?php _e( 'Comments are closed.' , 'wpAlpha' ); ?></p>
+        <?php endif; ?>
+ 
+    <?php endif; // have_comments() ?>
+ 
+    <?php comment_form(); ?>
+ 
 </div><!-- #comments -->
